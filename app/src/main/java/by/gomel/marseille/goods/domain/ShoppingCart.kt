1f036ec155809613fc79db905ea234d18b9ad3b6
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2019-04-22 23:00:13 +0300

    Refactor shopping cart

--- app/src/main/java/by/gomel/marseille/goods/domain/ShoppingCart.kt
+++ app/src/main/java/by/gomel/marseille/goods/domain/ShoppingCart.kt
@@ -1,27 +1,48 @@
 package by.gomel.marseille.goods.domain
 
-import by.gomel.marseille.goods.data.models.Service
+import by.gomel.marseille.goods.data.models.Product
+import io.reactivex.subjects.BehaviorSubject
 import java.io.Serializable
 
+
 class ShoppingCart : Serializable {
-    private val services: MutableSet<Service> = mutableSetOf()
+    
+    private val products: MutableMap<String, Product> = mutableMapOf()
+    private val count: MutableMap<String, Int> = mutableMapOf()
+    val amount: BehaviorSubject<Double> = BehaviorSubject.create()
 
-    fun add(vararg service: Service) = services.addAll(service)
-    fun add(services: MutableList<Service>) = this.services.addAll(services)
-    fun clear() = services.clear()
-    fun services() = services.toList()
-    fun getTotalAmount(): Pair<Double, Double> {
-        var minPrice = 0.0
-        var maxPrice = 0.0
-
-        services.forEach { service ->
-            minPrice += service.minPrice
-            maxPrice += if (service.maxPrice > 0) service.maxPrice else service.minPrice
+    fun add(vararg products: Product) {
+        products.forEach { product ->
+            if (this.products.containsKey(product.uid)) {
+                this.count[product.uid] = this.count[product.uid]!! + 1
+            } else {
+                this.products[product.uid] = product
+                this.count[product.uid] = 1
+            }
         }
-
-        return minPrice to maxPrice
+        updateAmount()
     }
 
-    fun getTotalAmountString() = "${getTotalAmount().first}" +
-            if (getTotalAmount().second > 0) " - ${getTotalAmount().second}" else ""
+    fun remove(vararg products: Product) {
+        products.forEach { product ->
+            this.products.remove(product.uid)
+            this.count.remove(product.uid)
+        }
+        updateAmount()
+    }
+
+    fun clear() {
+        products.clear()
+        count.clear()
+        updateAmount()
+    }
+
+    private fun updateAmount() {
+        var amount = 0.0
+        products.values.forEach { product ->
+            amount += product.price * count[product.uid]!!
+        }
+        this.amount.onNext(amount)
+    }
+
 }
